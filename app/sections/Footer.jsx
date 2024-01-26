10bd99d7b147a997a0d2159a9f6929d16e2689e1
Romain DELIMAL <rom.delimal@gmail.com>
2024-01-26 21:27:06 +0100

    removed yml lines for GH-pages

--- app/sections/Footer.jsx
+++ app/sections/Footer.jsx
@@ -41,7 +41,7 @@ export default function Footer() {
         </div>
 
         <div className="w-fit h-fit p-4">
-          <p>© 2023, Romain DELIMAL</p>
+          <p>© 2024, Romain DELIMAL</p>
         </div>
       </section>
 
